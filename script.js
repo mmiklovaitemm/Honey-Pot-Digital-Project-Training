cc6ea63f6c3e88c97485d16d2eed3007e74c0391
mmiklovaitemm <136790656+mmiklovaitemm@users.noreply.github.com>
2024-03-26 16:52:34 +0200

    dropdown width responsive update

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ $(document).ready(function(){
         $(".dropdown-content").hide();
     });
 });
+
+// Hide the dropdown by default
+$(document).ready(function(){
+    $(".dropdown-content").hide();
+});
+
+// pasiekiame mygtuko ir dropdown elementus
+var button = document.querySelector('.resources-button');
+var dropdown = document.querySelector('.dropdown-content');
+// pridedame paspaudimo įvykį mygtukui
+button.addEventListener('click', function() {
+
+  // Toggle klasės pridėjimą/šalinimą mygtukui
+  button.classList.toggle('active');
+
+  // Toggle klasės pridėjimą/šalinimą dropdown'ui
+  dropdown.classList.toggle('active');
+  
+  // Toggle klasės pridėjimą/šalinimą mygtukui, kai dropdown yra aktyvuotas
+  if (dropdown.classList.contains('active')) {
+    button.classList.add('active');
+  } else {
+    button.classList.remove('active');
+  }
+});
